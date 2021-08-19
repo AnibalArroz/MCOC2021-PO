bae91215922645d01b167b7e348de89db160b137
AnibalArroz <88512479+AnibalArroz@users.noreply.github.com>
2021-08-19 09:53:25 -0400

    Add files via upload

---
+++ timing_inv_caso_3_double.py
@@ -0,0 +1,85 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug 19 00:34:16 2021
+
+@author: Nicolas
+"""
+
+
+import numpy as np
+from time import perf_counter
+from numpy import half, single, double, longdouble, zeros, eye
+from numpy import float16, float32, float64
+from scipy.linalg import inv
+from laplaciana import laplaciana
+import matplotlib.pylab as plt
+
+
+
+N = 1000
+
+Ns = [10, 15, 20, 30, 40, 50, 60, 70, 80, 100, 130, 160, 200, 300, 400, 500, 650, 800, 900, 1000, 1500, 2000, 2500, 3500, 5000, 6000, 8000, 10000]
+
+
+t0 = perf_counter()
+Ms = []
+dt = []
+
+f = open("scipy_True_double.txt", "a")
+
+
+
+for N in Ns:
+    
+    # A = zeros((N, N))
+    # Am1 = inv(A)
+    
+    t1 = perf_counter()
+    A = laplaciana(N, dtype=double)
+    t2 = perf_counter()
+    
+    #print (f"{A}")
+    #exit(A)
+    
+    Aml = inv(A, overwrite_a=True)
+    t3 = perf_counter()
+    
+    #print (Aml)
+    
+    
+    dt_ensamblaje = t2 - t1
+    dt_inversion = t3 - t2
+    
+    dt.append(dt_inversion)
+    
+    bytes_total = (A.nbytes + Aml.nbytes)*(10**(-6))
+    
+    Ms.append(bytes_total)
+    
+    f.write(f"Matriz de {N}x{N}"+"\n")
+    f.write(f"Uso memoria: {bytes_total} bytes MB"+"\n")
+    f.write(f"Tiempo inversion: {dt_inversion} s"+"\n")
+    
+    
+    print ("Matriz de ", N)
+    print(f"Uso memoria: {bytes_total} bytes MB")   
+    #print(f"Tiempo ensamblaje: {dt_ensamblaje} s")
+    print(f"Tiempo inversion: {dt_inversion} s")
+    
+f.close()
+
+
+plt.figure(1)
+plt.subplot(2, 1, 1)
+#plt.loglog(Ms, marker = "o")
+plt.ylabel("Tiempo Transcurrido (s)")
+plt.title("Rendimiento inv")
+plt.loglog(Ms, dt,  marker = "o")
+plt.grid(True)
+
+plt.show()
+
+t4 = perf_counter()
+
+dt_total= t4 - t0
+print(f"Tiempo ejecucion: {dt_total} s")
